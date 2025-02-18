39bed07b4d76a88c27d30892f71e6b9dda6ac779
shaban <146794146+sarzhh@users.noreply.github.com>
2025-02-18 01:00:12 +0000

    template

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import pluginPrettier from 'eslint-plugin-prettier';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    languageOptions: {
+      globals: globals.browser,
+      sourceType: 'module',
+    },
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+  pluginJs.configs.recommended,
+  eslintConfigPrettier,
+];
